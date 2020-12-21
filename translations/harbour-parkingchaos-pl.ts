17a75e6947b249105a6f5687df7f9a3d55ab864f
A <hucul@o2.pl>
2020-12-21 13:38:44 +0100

    Create harbour-parkingchaos-pl.ts
    polish translation

---
+++ translations/harbour-parkingchaos-pl.ts
@@ -0,0 +1,160 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About this app</source>
+        <translation>O tej aplikacji</translation>
+    </message>
+    <message>
+        <source>Author</source>
+        <translation>Autor</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation>Versja %1</translation>
+    </message>
+    <message>
+        <source>Development</source>
+        <translation>Rozwój</translation>
+    </message>
+    <message>
+        <source>&lt;i&gt;show contributors &lt;/i&gt;</source>
+        <translation>&lt;i&gt;pokaż współtwórców &lt;/i&gt;</translation>
+    </message>
+    <message>
+        <source>Website</source>
+        <translation>Strona internetowa</translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation>Licencja</translation>
+    </message>
+    <message>
+        <source>&lt;i&gt;show license &lt;/i&gt;</source>
+        <translation>&lt;i&gt;pokaż licencję &lt;/i&gt;</translation>
+    </message>
+    <message>
+        <source>Source Code</source>
+        <translation>Kod źródłowy</translation>
+    </message>
+</context>
+<context>
+    <name>BoardPage</name>
+    <message>
+        <source>Level selection</source>
+        <translation>Wybór poziomu</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>O</translation>
+    </message>
+    <message>
+        <source>Parking Chaos</source>
+        <translation>Parking Chaos</translation>
+    </message>
+</context>
+<context>
+    <name>ContributorsPage</name>
+    <message>
+        <source>Contributors</source>
+        <translation>Wspołtwórcy</translation>
+    </message>
+    <message>
+        <source>Development</source>
+        <translation>Rozwój</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation>Tłumacze</translation>
+    </message>
+</context>
+<context>
+    <name>InfoLabel</name>
+    <message>
+        <source>Level %1</source>
+        <translation>Poziom %1</translation>
+    </message>
+    <message>
+        <source>Moves: %1 / %2</source>
+        <translation>Ruchy: %1 / %2</translation>
+    </message>
+    <message>
+        <source>Moves: %1</source>
+        <translation>Ruchy: %1</translation>
+    </message>
+</context>
+<context>
+    <name>LevelsPage</name>
+    <message>
+        <source>Levels</source>
+        <translation>Poziomy</translation>
+    </message>
+</context>
+<context>
+    <name>LicensePage</name>
+    <message>
+        <source>License</source>
+        <translation>Licencja</translation>
+    </message>
+</context>
+<context>
+    <name>about</name>
+    <message>
+        <source>Programming</source>
+        <translation>Programowanie</translation>
+    </message>
+    <message>
+        <source>Graphics Design</source>
+        <translation>Grafika</translation>
+    </message>
+    <message>
+        <source>ParkMeeCrazy</source>
+        <translation>ParkMeCrazy</translation>
+    </message>
+    <message>
+        <source>Rush hour in your parking lot!&lt;br&gt;&lt;br&gt;&lt;small&gt;&lt;em&gt;Parking Chaos&lt;/em&gt; is a clone of the famous “Rush Hour” or “Traffic Jam” game, written from scratch based on &lt;em&gt;ParkMeeCrazy&lt;/em&gt;.&lt;br&gt;Move the red tractor to the exit on the right by dragging others out of the way. Horizontal cars can only move left and right, vertical ones can only move up and down.&lt;/small&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Acknowledgements</source>
+        <translation>Podziękowanie</translation>
+    </message>
+    <message>
+        <source>&lt;small&gt;&lt;em&gt;Parking Chaos&lt;/em&gt; uses levels and graphics based on data from &lt;em&gt;ParkMeeCrazy&lt;/em&gt;, which is released under the terms of the GNU GPL v3+.&lt;/small&gt;&lt;br&gt;Thank you!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Swedish</source>
+        <translation>szwedzki</translation>
+    </message>
+    <message>
+        <source>English, German</source>
+        <translation>angielski, niemiecki</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-parkingchaos</name>
+    <message>
+        <source>Won!</source>
+        <translation>Zwycięstwo!</translation>
+    </message>
+    <message>
+        <source>Restart</source>
+        <translation>Restart</translation>
+    </message>
+    <message>
+        <source>Next level</source>
+        <translation>Kolejny poziom</translation>
+    </message>
+    <message numerus="yes">
+        <source>You completed this level in %L1 move(s)!</source>
+        <translation>
+            <numerusform>Ukończyłeś ten poziom w %L1 ruchu!</numerusform>
+            <numerusform>Ukończyłeś ten poziom w %L1 ruchach!</numerusform>
+            <numerusform>Ukończyłeś ten poziom w %L1 ruchach!</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
