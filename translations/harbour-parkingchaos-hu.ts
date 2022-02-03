d43cb73c5202be80cf8cd19a453d69d1c3e6afcc
1Zgp <36508793+1Zgp@users.noreply.github.com>
2022-02-03 14:55:21 +0100

    Add files via upload

---
+++ translations/harbour-parkingchaos-hu.ts
@@ -0,0 +1,162 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu" sourcelanguage="en_US">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About this app</source>
+        <translation>Az alkalmazásról</translation>
+    </message>
+    <message>
+        <source>Author</source>
+        <translation>Készítő</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation>%1 verzió</translation>
+    </message>
+    <message>
+        <source>Development</source>
+        <translation>Fejlesztés</translation>
+    </message>
+    <message>
+        <source>&lt;i&gt;show contributors &lt;/i&gt;</source>
+        <translation>&lt;i&gt;közreműködők megjelenítése &lt;/i&gt;</translation>
+    </message>
+    <message>
+        <source>Website</source>
+        <translation>Weboldal</translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation>Licenc</translation>
+    </message>
+    <message>
+        <source>&lt;i&gt;show license &lt;/i&gt;</source>
+        <translation>&lt;i&gt;licenc megjelenítése &lt;/i&gt;</translation>
+    </message>
+    <message>
+        <source>Source Code</source>
+        <translation>Forráskód</translation>
+    </message>
+</context>
+<context>
+    <name>BoardPage</name>
+    <message>
+        <source>Level selection</source>
+        <translation>Szintválasztó</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Névjegy</translation>
+    </message>
+    <message>
+        <source>Parking Chaos</source>
+        <translation>Parking Chaos</translation>
+    </message>
+</context>
+<context>
+    <name>ContributorsPage</name>
+    <message>
+        <source>Contributors</source>
+        <translation>Közreműködők</translation>
+    </message>
+    <message>
+        <source>Development</source>
+        <translation>Fejlesztés</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation>Fordítások</translation>
+    </message>
+</context>
+<context>
+    <name>InfoLabel</name>
+    <message>
+        <source>Level %1</source>
+        <translation>%1. szint</translation>
+    </message>
+    <message>
+        <source>Moves: %1 / %2</source>
+        <translation>Lépések: %1 / %2</translation>
+    </message>
+    <message>
+        <source>Moves: %1</source>
+        <translation>%1 lépés</translation>
+    </message>
+</context>
+<context>
+    <name>LevelsPage</name>
+    <message>
+        <source>Levels</source>
+        <translation>Szintek</translation>
+    </message>
+</context>
+<context>
+    <name>LicensePage</name>
+    <message>
+        <source>License</source>
+        <translation>Licenc</translation>
+    </message>
+</context>
+<context>
+    <name>about</name>
+    <message>
+        <source>Programming</source>
+        <translation>Programozás</translation>
+    </message>
+    <message>
+        <source>Graphics Design</source>
+        <translation>Grafikai tervezés</translation>
+    </message>
+    <message>
+        <source>ParkMeeCrazy</source>
+        <translation>ParkMeeCrazy</translation>
+    </message>
+    <message>
+        <source>Rush hour in your parking lot!&lt;br&gt;&lt;br&gt;&lt;small&gt;&lt;em&gt;Parking Chaos&lt;/em&gt; is a clone of the famous “Rush Hour” or “Traffic Jam” game, written from scratch based on &lt;em&gt;ParkMeeCrazy&lt;/em&gt;.&lt;br&gt;Move the red tractor to the exit on the right by dragging others out of the way. Horizontal cars can only move left and right, vertical ones can only move up and down.&lt;/small&gt;</source>
+        <translation>Csúcsforgalom a parkolódban!&lt;br&gt;&lt;br&gt;&lt;small&gt;A &lt;em&gt;Parking Chaos&lt;/em&gt; a “Rush Hour” vagy “Traffic Jam” nevű játékok klónja, a semmiből íródott a &lt;em&gt;ParkMeeCrazy&lt;em&gt; alapján.&lt;br&gt;Mozgasd a piros traktort a jobb oldali kijárathoz úgy, hogy a többieket félrehúzod az útból. A vízszintes járművek csak jobbra és balra mozoghatnak, a függőlegesek csak felfelé és lefelé.&lt;/small&gt;</translation>
+    </message>
+    <message>
+        <source>Acknowledgements</source>
+        <translation>Köszönetnyilvánítás</translation>
+    </message>
+    <message>
+        <source>&lt;small&gt;&lt;em&gt;Parking Chaos&lt;/em&gt; uses levels and graphics based on data from &lt;em&gt;ParkMeeCrazy&lt;/em&gt;, which is released under the terms of the GNU GPL v3+.&lt;/small&gt;&lt;br&gt;Thank you!</source>
+        <translation>&lt;small&gt;A&lt;em&gt;Parking Chaos&lt;/em&gt; a &lt;em&gt;ParkMeeCrazy&lt;/em&gt;-n alapuló grafikát és szinteket használ, melyek a GNU GPL v3+ hatálya alatt kelültek kiadásra.&lt;/small&gt;&lt;br&gt;Köszönöm!</translation>
+    </message>
+    <message>
+        <source>Swedish</source>
+        <translation>Svéd</translation>
+    </message>
+    <message>
+        <source>English, German</source>
+        <translation>Angol, német</translation>
+    </message>
+    <message>
+        <source>Hungarian</source>
+        <translation>Magyar</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-parkingchaos</name>
+    <message>
+        <source>Won!</source>
+        <translation>Nyertél!</translation>
+    </message>
+    <message>
+        <source>Restart</source>
+        <translation>Újra</translation>
+    </message>
+    <message>
+        <source>Next level</source>
+        <translation>Következő szint</translation>
+    </message>
+    <message numerus="yes">
+        <source>You completed this level in %L1 move(s)!</source>
+        <translation>
+            <numerusform>Ezt a szintet %Ln lépésből oldottad meg!</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
