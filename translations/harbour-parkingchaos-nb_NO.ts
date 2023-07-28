a281d67d0eb5bfbf53d9441e48318624031b6ec8
Allan Nordhøy <epost@anotheragency.no>
2023-07-28 07:07:42 +0200

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 96.4% (54 of 56 strings)
    
    Added translation using Weblate (Norwegian Bokmål)
    
    Co-authored-by: Allan Nordhøy <epost@anotheragency.no>
    Translate-URL: https://hosted.weblate.org/projects/harbour-parkingchaos/translations/nb_NO/
    Translation: Parking Chaos for SailfishOS/App Translations

---
+++ translations/harbour-parkingchaos-nb_NO.ts
@@ -0,0 +1,327 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="nb_NO">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="95"/>
+        <source>Development</source>
+        <translation>Utvikling</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="86"/>
+        <source>Website</source>
+        <translation>Nettside</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="33"/>
+        <source>Parking Chaos</source>
+        <translation>Parkeringskaos</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="98"/>
+        <source>Programming</source>
+        <translation>Programmering</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="102"/>
+        <source>Graphics Design</source>
+        <translation>Grafikkdesign</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="108"/>
+        <source>Translations</source>
+        <translation>Oversettelser</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="110"/>
+        <source>Swedish</source>
+        <translation>Svensk</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="111"/>
+        <source>Polish</source>
+        <translation>Polsk</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="112"/>
+        <source>Russian</source>
+        <translation>Russisk</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="113"/>
+        <source>Hungarian</source>
+        <translation>Ungarsk</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="114"/>
+        <source>English, German</source>
+        <translation>Engelsk, tysk</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="53"/>
+        <source>Rush hour in your parking lot!&lt;br&gt;&lt;br&gt; &lt;i&gt;Parking Chaos&lt;/i&gt; is a clone of the famous “Rush Hour” or “Traffic Jam” game, written from scratch based on &lt;i&gt;ParkMeeCrazy&lt;/i&gt;.&lt;br&gt;&lt;br&gt; Move the red tractor to the exit on the right by dragging others out of the way. Horizontal cars can only move left and right, vertical ones can only move up and down.</source>
+        <translation type="unfinished">Rushtid på parkeringsplassen!&lt;br&gt;&lt;br&gt; &lt;i&gt;Parkeringskaos&lt;/i&gt; er en klone av det kjente «Rush Hour»-, eller «Traffic Jam»-spillet. Det er skrevet fra bunnen av basert på &lt;i&gt;ParkMeeCrazy&lt;/i&gt;.&lt;br&gt;&lt;br&gt;Flytt den røde traktoren til utkjøringen på høyre side, gjennom å dra de andre kjøretøyene. Horisentale kjøretøy kan kun flyttes til venstre og høyre, og vertikale kun opp- og nedover.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="67"/>
+        <source>Graphics and levels are based on data from ParkMeeCrazy.</source>
+        <translation>Grafikk og nivåer er basert på data fra ParkMeeCrazy.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="80"/>
+        <source>Acknowledgements</source>
+        <translation>Kunngjøringer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="81"/>
+        <source>&lt;i&gt;Parking Chaos&lt;/i&gt; uses levels and graphics based on data from &lt;i&gt;ParkMeeCrazy&lt;/i&gt;, which is released under the terms of the GNU GPL v3+.&lt;br&gt;Thank you!</source>
+        <translation>&lt;i&gt;Parkeringskaos&lt;/i&gt; bruker nivåer og grafikk basert på data fra &lt;i&gt;ParkMeeCrazy&lt;/i&gt;, som er utgitt under GNU GPL v3+.&lt;br&gt;Takk.</translation>
+    </message>
+</context>
+<context>
+    <name>BoardPage</name>
+    <message>
+        <location filename="../qml/pages/BoardPage.qml" line="29"/>
+        <source>Level selection</source>
+        <translation>Nivåvalg</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BoardPage.qml" line="25"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BoardPage.qml" line="40"/>
+        <source>Parking Chaos</source>
+        <translation>Parkeringskaos</translation>
+    </message>
+</context>
+<context>
+    <name>InfoLabel</name>
+    <message>
+        <location filename="../qml/components/InfoLabel.qml" line="20"/>
+        <source>Level %1</source>
+        <translation>Nivå %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/InfoLabel.qml" line="22"/>
+        <source>Moves: %1 / %2</source>
+        <translation>Forflyttninger: %1/%2</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/InfoLabel.qml" line="23"/>
+        <source>Moves: %1</source>
+        <translation>Forflyttninger: %1</translation>
+    </message>
+</context>
+<context>
+    <name>LevelsPage</name>
+    <message>
+        <location filename="../qml/pages/LevelsPage.qml" line="20"/>
+        <source>Levels</source>
+        <translation>Nivåer</translation>
+    </message>
+</context>
+<context>
+    <name>LicenseListPart</name>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/LicenseListPart.qml" line="119"/>
+        <source>License text</source>
+        <translation>Lisenstekst</translation>
+    </message>
+</context>
+<context>
+    <name>Opal.About</name>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="55"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="84"/>
+        <source>Version %1</source>
+        <translation>Versjon %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="100"/>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="22"/>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="31"/>
+        <source>Development</source>
+        <translation>Utvikling</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="103"/>
+        <source>show contributors</source>
+        <translation>vis bidragsytere</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="105"/>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="51"/>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="57"/>
+        <location filename="../qml/modules/Opal/About/private/LicenseListPart.qml" line="47"/>
+        <source>Homepage</source>
+        <translation>Hjemmeside</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="108"/>
+        <location filename="../qml/modules/Opal/About/private/ChangelogPage.qml" line="16"/>
+        <source>Changelog</source>
+        <translation>Endringslogg</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="111"/>
+        <source>Translations</source>
+        <translation>Oversettelser</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="114"/>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="50"/>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="58"/>
+        <location filename="../qml/modules/Opal/About/private/LicenseListPart.qml" line="50"/>
+        <source>Source Code</source>
+        <translation>Kildekode</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="123"/>
+        <source>Donations</source>
+        <translation>Donasjoner</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="129"/>
+        <source>License</source>
+        <translation>Lisens</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="134"/>
+        <source>show license(s)</source>
+        <translation>
+            <numerusform>vis lisens</numerusform>
+            <numerusform>vis lisenser</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/InfoSection.qml" line="15"/>
+        <source>show details</source>
+        <translation>vis detaljer</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="21"/>
+        <source>Contributors</source>
+        <translation>Bidragsytere</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="37"/>
+        <source>Acknowledgements</source>
+        <translation>Kunngjøringer</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="44"/>
+        <source>Thank you!</source>
+        <translation>Takk</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="48"/>
+        <location filename="../qml/modules/Opal/About/private/LicensePage.qml" line="31"/>
+        <source>Details</source>
+        <translation>Detaljer</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ExternalUrlPage.qml" line="15"/>
+        <source>External Link</source>
+        <translation>Ekstern lenke</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ExternalUrlPage.qml" line="32"/>
+        <source>Open in browser</source>
+        <translation type="unfinished">Åpne i nettleser</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ExternalUrlPage.qml" line="36"/>
+        <source>Copied to clipboard: %1</source>
+        <translation>Kopiert til utklippstavle: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ExternalUrlPage.qml" line="41"/>
+        <source>Copy to clipboard</source>
+        <translation>Kopier til utklippstavle</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/LicensePage.qml" line="25"/>
+        <source>Download license texts</source>
+        <translation>Last ned lisenstekster</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/modules/Opal/About/private/LicensePage.qml" line="31"/>
+        <source>License(s)</source>
+        <translation>
+            <numerusform>Lisens</numerusform>
+            <numerusform>Lisenser</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/LicensePage.qml" line="41"/>
+        <source>Note: please check the source code for most accurate information.</source>
+        <translation>Merk: Sjekk kildekoden for mest mulig sannferdig info.</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/ChangelogNews.qml" line="60"/>
+        <source>News</source>
+        <translation>Nyheter</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/ChangelogNews.qml" line="61"/>
+        <source>Changes since version %1</source>
+        <translation>Endringer siden versjon %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/LicenseListPart.qml" line="120"/>
+        <source>Please refer to &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</source>
+        <translation>Henvend dem til &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>Opal.About.Common</name>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/DonationsGroup.qml" line="7"/>
+        <source>If you want to support my work, you can buy me a cup of coffee.</source>
+        <translation>Hvis du ønsker å støtte arbeidet kan du spandere drikke.</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/DonationsGroup.qml" line="8"/>
+        <source>You can support this project by contributing, or by donating using any of these services.</source>
+        <translation>Du kan støtte dette prosjektet ved å bidra, eller ved å bruke noen av disse tjenestene.</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/DonationsGroup.qml" line="9"/>
+        <source>Your contributions to translations or code would be most welcome.</source>
+        <translation>Kildekodebidrag og oversettelser er også hjertelig velkomne.</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-parkingchaos</name>
+    <message>
+        <location filename="../qml/harbour-parkingchaos.qml" line="79"/>
+        <source>Won!</source>
+        <translation>Du vant.</translation>
+    </message>
+    <message>
+        <location filename="../qml/harbour-parkingchaos.qml" line="100"/>
+        <source>Restart</source>
+        <translation>Omstart</translation>
+    </message>
+    <message>
+        <location filename="../qml/harbour-parkingchaos.qml" line="104"/>
+        <source>Next level</source>
+        <translation>Neste nivå</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/harbour-parkingchaos.qml" line="88"/>
+        <source>You completed this level in %L1 move(s)!</source>
+        <translation>
+            <numerusform>Du fullførte nivået med %L1 forflyttning.</numerusform>
+            <numerusform>Du fullførte nivået med %L1 forflyttninger.</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
