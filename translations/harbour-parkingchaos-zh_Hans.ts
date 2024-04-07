3ad875d654af1abc8ef19c198151a5fd0322b009
复予 <clonewith@qq.com>
2024-04-07 07:07:35 +0200

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (56 of 56 strings)
    
    Translated using Weblate (Chinese (Simplified))
    
    Currently translated at 83.9% (47 of 56 strings)
    
    Added translation using Weblate (Chinese (Simplified))
    
    Co-authored-by: 复予 <clonewith@qq.com>
    Translate-URL: https://hosted.weblate.org/projects/harbour-parkingchaos/translations/zh_Hans/
    Translation: Parking Chaos for SailfishOS/App Translations

---
+++ translations/harbour-parkingchaos-zh_Hans.ts
@@ -0,0 +1,324 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="zh_Hans">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="95"/>
+        <source>Development</source>
+        <translation>开发</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="86"/>
+        <source>Website</source>
+        <translation>网站</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="33"/>
+        <source>Parking Chaos</source>
+        <translation>停车混乱</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="98"/>
+        <source>Programming</source>
+        <translation>编程</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="102"/>
+        <source>Graphics Design</source>
+        <translation>图形设计</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="108"/>
+        <source>Translations</source>
+        <translation>翻译</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="110"/>
+        <source>Swedish</source>
+        <translation>瑞典语</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="111"/>
+        <source>Polish</source>
+        <translation>波兰语</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="112"/>
+        <source>Russian</source>
+        <translation>俄语</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="113"/>
+        <source>Hungarian</source>
+        <translation>匈牙利语</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="114"/>
+        <source>English, German</source>
+        <translation>英语、德语</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="53"/>
+        <source>Rush hour in your parking lot!&lt;br&gt;&lt;br&gt; &lt;i&gt;Parking Chaos&lt;/i&gt; is a clone of the famous “Rush Hour” or “Traffic Jam” game, written from scratch based on &lt;i&gt;ParkMeeCrazy&lt;/i&gt;.&lt;br&gt;&lt;br&gt; Move the red tractor to the exit on the right by dragging others out of the way. Horizontal cars can only move left and right, vertical ones can only move up and down.</source>
+        <translation>你的停车场迎来了高峰期！&lt;br&gt;&lt;br&gt; &lt;i&gt;停车混乱 (Parking Chaos)&lt;/i&gt;是著名游戏“Rush Hour” 或 “Traffic Jam”的仿制版本， 基于 &lt;i&gt;ParkMeeCrazy&lt;/i&gt; 的工作从头制作。&lt;br&gt;&lt;br&gt;通过将其他挡路车辆移开，将红色拖拉机移动到右侧出口。横向车辆只可左右移动，纵向车辆只可上下移动。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="67"/>
+        <source>Graphics and levels are based on data from ParkMeeCrazy.</source>
+        <translation>图形与关卡基于 ParkMeeCrazy 的数据制成。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="80"/>
+        <source>Acknowledgements</source>
+        <translation>鸣谢</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="81"/>
+        <source>&lt;i&gt;Parking Chaos&lt;/i&gt; uses levels and graphics based on data from &lt;i&gt;ParkMeeCrazy&lt;/i&gt;, which is released under the terms of the GNU GPL v3+.&lt;br&gt;Thank you!</source>
+        <translation>&lt;i&gt;Parking Chaos&lt;/i&gt; 使用的是基于 &lt;i&gt;ParkMeeCrazy&lt;/i&gt; 的关卡与图形数据，在 GNU GPL v3+ 许可证下发布。&lt;br&gt;谢谢！</translation>
+    </message>
+</context>
+<context>
+    <name>BoardPage</name>
+    <message>
+        <location filename="../qml/pages/BoardPage.qml" line="29"/>
+        <source>Level selection</source>
+        <translation>关卡选择</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BoardPage.qml" line="25"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BoardPage.qml" line="40"/>
+        <source>Parking Chaos</source>
+        <translation>停车混乱</translation>
+    </message>
+</context>
+<context>
+    <name>InfoLabel</name>
+    <message>
+        <location filename="../qml/components/InfoLabel.qml" line="20"/>
+        <source>Level %1</source>
+        <translation>关卡 %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/InfoLabel.qml" line="22"/>
+        <source>Moves: %1 / %2</source>
+        <translation>步数：%1 / %2</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/InfoLabel.qml" line="23"/>
+        <source>Moves: %1</source>
+        <translation>步数：%1</translation>
+    </message>
+</context>
+<context>
+    <name>LevelsPage</name>
+    <message>
+        <location filename="../qml/pages/LevelsPage.qml" line="20"/>
+        <source>Levels</source>
+        <translation>关卡</translation>
+    </message>
+</context>
+<context>
+    <name>LicenseListPart</name>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/LicenseListPart.qml" line="119"/>
+        <source>License text</source>
+        <translation>许可证文本</translation>
+    </message>
+</context>
+<context>
+    <name>Opal.About</name>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="55"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="84"/>
+        <source>Version %1</source>
+        <translation>版本 %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="100"/>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="22"/>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="31"/>
+        <source>Development</source>
+        <translation>开发</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="103"/>
+        <source>show contributors</source>
+        <translation>显示贡献者</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="105"/>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="51"/>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="57"/>
+        <location filename="../qml/modules/Opal/About/private/LicenseListPart.qml" line="47"/>
+        <source>Homepage</source>
+        <translation>主页</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="108"/>
+        <location filename="../qml/modules/Opal/About/private/ChangelogPage.qml" line="16"/>
+        <source>Changelog</source>
+        <translation>更新日志</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="111"/>
+        <source>Translations</source>
+        <translation>翻译</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="114"/>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="50"/>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="58"/>
+        <location filename="../qml/modules/Opal/About/private/LicenseListPart.qml" line="50"/>
+        <source>Source Code</source>
+        <translation>源代码</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="123"/>
+        <source>Donations</source>
+        <translation>捐赠</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="129"/>
+        <source>License</source>
+        <translation>许可证</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="134"/>
+        <source>show license(s)</source>
+        <translation>
+            <numerusform>显示许可证</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/InfoSection.qml" line="15"/>
+        <source>show details</source>
+        <translation>显示详情</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="21"/>
+        <source>Contributors</source>
+        <translation>贡献者</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="37"/>
+        <source>Acknowledgements</source>
+        <translation>鸣谢</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="44"/>
+        <source>Thank you!</source>
+        <translation>谢谢！</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ContributorsPage.qml" line="48"/>
+        <location filename="../qml/modules/Opal/About/private/LicensePage.qml" line="31"/>
+        <source>Details</source>
+        <translation>详情</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ExternalUrlPage.qml" line="15"/>
+        <source>External Link</source>
+        <translation>外部链接</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ExternalUrlPage.qml" line="32"/>
+        <source>Open in browser</source>
+        <translation>在浏览器中打开</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ExternalUrlPage.qml" line="36"/>
+        <source>Copied to clipboard: %1</source>
+        <translation>已复制到剪贴板：%1</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/ExternalUrlPage.qml" line="41"/>
+        <source>Copy to clipboard</source>
+        <translation>复制到剪贴板</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/LicensePage.qml" line="25"/>
+        <source>Download license texts</source>
+        <translation>下载许可证文本</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/modules/Opal/About/private/LicensePage.qml" line="31"/>
+        <source>License(s)</source>
+        <translation>
+            <numerusform>许可证</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/LicensePage.qml" line="41"/>
+        <source>Note: please check the source code for most accurate information.</source>
+        <translation>注：请查看源代码以获取更准确的信息。</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/ChangelogNews.qml" line="60"/>
+        <source>News</source>
+        <translation>新闻</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/ChangelogNews.qml" line="61"/>
+        <source>Changes since version %1</source>
+        <translation>自版本 %1 的变化</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/LicenseListPart.qml" line="120"/>
+        <source>Please refer to &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</source>
+        <translation>请参见&lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>Opal.About.Common</name>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/DonationsGroup.qml" line="7"/>
+        <source>If you want to support my work, you can buy me a cup of coffee.</source>
+        <translation>如果你想支持我的工作，可以给我买一杯咖啡。</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/DonationsGroup.qml" line="8"/>
+        <source>You can support this project by contributing, or by donating using any of these services.</source>
+        <translation>你可以做出贡献来支持这个项目，也可以用以下服务捐赠。</translation>
+    </message>
+    <message>
+        <location filename="../qml/modules/Opal/About/private/DonationsGroup.qml" line="9"/>
+        <source>Your contributions to translations or code would be most welcome.</source>
+        <translation>欢迎你对翻译与代码做出贡献。</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-parkingchaos</name>
+    <message>
+        <location filename="../qml/harbour-parkingchaos.qml" line="79"/>
+        <source>Won!</source>
+        <translation>获胜！</translation>
+    </message>
+    <message>
+        <location filename="../qml/harbour-parkingchaos.qml" line="100"/>
+        <source>Restart</source>
+        <translation>重试</translation>
+    </message>
+    <message>
+        <location filename="../qml/harbour-parkingchaos.qml" line="104"/>
+        <source>Next level</source>
+        <translation>下一关</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/harbour-parkingchaos.qml" line="88"/>
+        <source>You completed this level in %L1 move(s)!</source>
+        <translation>
+            <numerusform>你用 %L1 完成了此关卡！</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
