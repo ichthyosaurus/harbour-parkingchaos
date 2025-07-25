65de47bdce7419f433edabd57c25b1e0a596c5be
Burak Hüseyin Ekseli <burakekseli@yandex.com>
2025-07-25 09:03:50 +0200

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (86 of 86 strings)
    
    Translated using Weblate (Turkish)
    
    Currently translated at 100.0% (86 of 86 strings)
    
    Translated using Weblate (Turkish)
    
    Currently translated at 100.0% (86 of 86 strings)
    
    Translated using Weblate (Turkish)
    
    Currently translated at 100.0% (86 of 86 strings)
    
    Translated using Weblate (Turkish)
    
    Currently translated at 100.0% (86 of 86 strings)
    
    Co-authored-by: Burak Hüseyin Ekseli <burakekseli@yandex.com>
    Translate-URL: https://hosted.weblate.org/projects/harbour-parkingchaos/translations/tr/
    Translation: Parking Chaos for SailfishOS/App: Parking Chaos

--- translations/harbour-parkingchaos-tr.ts
+++ translations/harbour-parkingchaos-tr.ts
@@ -11,12 +11,12 @@
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="78"/>
         <source>Website</source>
-        <translation type="unfinished"></translation>
+        <translation>İnternet sayfası</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="25"/>
         <source>Parking Chaos</source>
-        <translation type="unfinished"></translation>
+        <translation>Parking Chaos</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="90"/>
@@ -26,7 +26,7 @@
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="94"/>
         <source>Graphics Design</source>
-        <translation type="unfinished"></translation>
+        <translation>Grafik Tasarım</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="101"/>
@@ -76,7 +76,7 @@
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="130"/>
         <source>Norwegian Bokmål</source>
-        <translation type="unfinished"></translation>
+        <translation>Norveççe Bokmål</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="136"/>
@@ -86,12 +86,12 @@
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="45"/>
         <source>Rush hour in your parking lot!&lt;br&gt;&lt;br&gt; &lt;i&gt;Parking Chaos&lt;/i&gt; is a clone of the famous “Rush Hour” or “Traffic Jam” game, written from scratch based on &lt;i&gt;ParkMeeCrazy&lt;/i&gt;.&lt;br&gt;&lt;br&gt; Move the red tractor to the exit on the right by dragging others out of the way. Horizontal cars can only move left and right, vertical ones can only move up and down.</source>
-        <translation type="unfinished"></translation>
+        <translation>Park yerinizde iş vakti &lt;br&gt;&lt;br&gt; &lt;i&gt;Parking Chaos&lt;/i&gt; oyunu, &lt;i&gt;ParkMeeCrazy&lt;/i&gt; tarafından yapılan meşhur “Rush Hour” ve “Traffic Jam” oyunlarının bir kopyasıdır. Kırmızı traktörü, diğer araçları yoldan çekecek sağ taraftaki çıkışa götürün. Yatay konumdaki araçlar yalnızca sağa ve sola, dikey araçlar ise yukarı ve aşağı gidebilir.</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="60"/>
         <source>Graphics and levels are based on data from ParkMeeCrazy.</source>
-        <translation type="unfinished"></translation>
+        <translation>Grafikler ve seviyeler ParkMeeCrazy&apos;den alınan verilere dayanmaktadır.</translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="72"/>
@@ -101,7 +101,7 @@
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="73"/>
         <source>&lt;i&gt;Parking Chaos&lt;/i&gt; uses levels and graphics based on data from &lt;i&gt;ParkMeeCrazy&lt;/i&gt;, which is released under the terms of the GNU GPL v3+.&lt;br&gt;Thank you!</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;i&gt;Parking Chaos&lt;/i&gt; GNU GPL v3+ lisansıyla yayınlanmış olan &lt;i&gt;ParkMeeCrazy&lt;/i&gt;, oyununun seviyeleri ve grafiklerini kullanmaktadır. Teşekkürler!</translation>
     </message>
 </context>
 <context>
@@ -109,7 +109,7 @@
     <message>
         <location filename="../qml/modules/Opal/SupportMe/AskForSupport.qml" line="11"/>
         <source>Please take a moment to consider if you can contribute to this project in one way or another.</source>
-        <translation type="unfinished"></translation>
+        <translation>Lütfen bu projeye bir şekilde katkıda bulunup bulunamayacağınızı düşünmek için bir dakikanızı ayırın.</translation>
     </message>
 </context>
 <context>
@@ -117,7 +117,7 @@
     <message>
         <location filename="../qml/pages/BoardPage.qml" line="29"/>
         <source>Level selection</source>
-        <translation type="unfinished"></translation>
+        <translation>Seviye seçimi</translation>
     </message>
     <message>
         <location filename="../qml/pages/BoardPage.qml" line="25"/>
@@ -127,7 +127,7 @@
     <message>
         <location filename="../qml/pages/BoardPage.qml" line="44"/>
         <source>Parking Chaos</source>
-        <translation type="unfinished"></translation>
+        <translation>Parking Chaos</translation>
     </message>
 </context>
 <context>
@@ -135,12 +135,12 @@
     <message>
         <location filename="../qml/modules/Opal/SupportMe/DetailsDrawer.qml" line="53"/>
         <source>show less</source>
-        <translation type="unfinished"></translation>
+        <translation>daha az göster</translation>
     </message>
     <message>
         <location filename="../qml/modules/Opal/SupportMe/DetailsDrawer.qml" line="53"/>
         <source>show more</source>
-        <translation type="unfinished"></translation>
+        <translation>daha fazla göster</translation>
     </message>
 </context>
 <context>
@@ -148,17 +148,17 @@
     <message>
         <location filename="../qml/components/InfoLabel.qml" line="38"/>
         <source>Level %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Seviye %1</translation>
     </message>
     <message>
         <location filename="../qml/components/InfoLabel.qml" line="64"/>
         <source>%1 / %2 moves</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 / %2 hareket</translation>
     </message>
     <message>
         <location filename="../qml/components/InfoLabel.qml" line="65"/>
         <source>%1 moves</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 hareket</translation>
     </message>
 </context>
 <context>
@@ -166,7 +166,7 @@
     <message>
         <location filename="../qml/pages/LevelsPage.qml" line="20"/>
         <source>Levels</source>
-        <translation type="unfinished"></translation>
+        <translation>Seviyeler</translation>
     </message>
 </context>
 <context>
@@ -174,7 +174,7 @@
     <message>
         <location filename="../qml/modules/Opal/About/private/LicenseListPart.qml" line="119"/>
         <source>License text</source>
-        <translation type="unfinished"></translation>
+        <translation>Lisans metni</translation>
     </message>
 </context>
 <context>
@@ -182,62 +182,62 @@
     <message>
         <location filename="../qml/MySupportDialog.qml" line="14"/>
         <source>Donate on Liberapay</source>
-        <translation type="unfinished"></translation>
+        <translation>Liberapay ile bağışta bulun</translation>
     </message>
     <message>
         <location filename="../qml/MySupportDialog.qml" line="15"/>
         <source>Pay the amount of a cup of coffee, a slice of pizza, or a ticket to the theater.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bir fincan kahve, bir dilim pizza ya da bir tiyatro bileti kadar ödeme yapın.</translation>
     </message>
     <message>
         <location filename="../qml/MySupportDialog.qml" line="22"/>
         <source>Translate on Weblate</source>
-        <translation type="unfinished"></translation>
+        <translation>Weblate üzerinde çevirİn</translation>
     </message>
     <message>
         <location filename="../qml/MySupportDialog.qml" line="23"/>
         <source>Help with translating this app in as many languages as possible.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu uygulamanın mümkün olduğunca çok dile çevrilmesine yardımcı olun.</translation>
     </message>
     <message>
         <location filename="../qml/MySupportDialog.qml" line="30"/>
         <source>Develop on Github</source>
-        <translation type="unfinished"></translation>
+        <translation>Github üzerinde geliştirin</translation>
     </message>
     <message>
         <location filename="../qml/MySupportDialog.qml" line="31"/>
         <source>Support with maintenance and packaging, write code, or provide valuable bug reports.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bakım ve paketleme konusunda destek verin, kod yazın veya değerli hata raporları sağlayın.</translation>
     </message>
     <message>
         <location filename="../qml/MySupportDialog.qml" line="37"/>
         <source>Why should you care?</source>
-        <translation type="unfinished"></translation>
+        <translation>Neden önemsemelisiniz?</translation>
     </message>
     <message>
         <location filename="../qml/MySupportDialog.qml" line="40"/>
         <source>This project is built with love and passion by a single developer in their spare time, and is provided to you free of charge.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu proje, tek bir geliştirici tarafından boş zamanlarında sevgi ve tutkuyla inşa edilmiş ve size ücretsiz olarak sunulmuştur.</translation>
     </message>
     <message>
         <location filename="../qml/MySupportDialog.qml" line="46"/>
         <source>I develop Free Software because I am convinced that it is the ethical thing to do - and it is a fun hobby. However, developing software takes a lot of time and effort. As Sailfish and living in general costs money, I need your support to be able to spend time on non-paying projects like this.</source>
-        <translation type="unfinished"></translation>
+        <translation>Özgür Yazılım geliştiriyorum çünkü bunun yapılması gereken etik bir şey olduğuna inanıyorum - ve eğlenceli bir hobi. Ancak, yazılım geliştirmek çok fazla zaman ve çaba gerektiriyor. Sailfish ve genel olarak yaşamak paraya mal olduğu için, bunun gibi para getirmeyen projelere zaman ayırabilmek için desteğinize ihtiyacım var.</translation>
     </message>
     <message>
         <location filename="../qml/MySupportDialog.qml" line="56"/>
         <source>Why donate?</source>
-        <translation type="unfinished"></translation>
+        <translation>Neden bağış yapmalısınız?</translation>
     </message>
     <message>
         <location filename="../qml/MySupportDialog.qml" line="59"/>
         <source>Jolla raised prices and is trying to force developers (who work for free) to pay rent for Sailfish.</source>
-        <translation type="unfinished"></translation>
+        <translation>Jolla fiyatları yükseltti ve geliştiricileri (ücretsiz çalışan) Sailfish için kira ödemeye zorlamaya çalışıyor.</translation>
     </message>
     <message>
         <location filename="../qml/MySupportDialog.qml" line="64"/>
         <source>If you can afford it, donating is the easiest way to ensure that I can continue working on apps for Sailfish. Any amount is appreciated, be it a cup of coffee, a slice of pizza, or more.</source>
-        <translation type="unfinished"></translation>
+        <translation>Eğer gücünüz yetiyorsa, Sailfish için uygulamalar üzerinde çalışmaya devam edebilmemi sağlamanın en kolay yolu bağış yapmaktır. Bir fincan kahve, bir dilim pizza ya da daha fazlası, her türlü miktar takdire şayandır.</translation>
     </message>
 </context>
 <context>
@@ -276,7 +276,7 @@
         <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="109"/>
         <location filename="../qml/modules/Opal/About/private/ChangelogPage.qml" line="16"/>
         <source>Changelog</source>
-        <translation type="unfinished"></translation>
+        <translation>Değişim günlüğü</translation>
     </message>
     <message>
         <location filename="../qml/modules/Opal/About/AboutPageBase.qml" line="112"/>
@@ -374,17 +374,17 @@
     <message>
         <location filename="../qml/modules/Opal/About/ChangelogNews.qml" line="61"/>
         <source>News</source>
-        <translation type="unfinished"></translation>
+        <translation>Haberler</translation>
     </message>
     <message>
         <location filename="../qml/modules/Opal/About/ChangelogNews.qml" line="62"/>
         <source>Changes since version %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Şu versiyondan beri değişiklikler %1</translation>
     </message>
     <message>
         <location filename="../qml/modules/Opal/About/private/LicenseListPart.qml" line="120"/>
         <source>Please refer to &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Lütfen &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt; bölümüne bakın</translation>
     </message>
 </context>
 <context>
@@ -420,7 +420,7 @@
     <message>
         <location filename="../qml/modules/Opal/SupportMe/private/LinkHandler/private/ExternalUrlPage.qml" line="32"/>
         <source>Open externally</source>
-        <translation type="unfinished"></translation>
+        <translation>Harici olarak aç</translation>
     </message>
     <message>
         <location filename="../qml/modules/Opal/SupportMe/private/LinkHandler/private/ExternalUrlPage.qml" line="36"/>
@@ -438,27 +438,27 @@
     <message>
         <location filename="../qml/modules/Opal/SupportMe/SupportDialog.qml" line="9"/>
         <source>Hi there!</source>
-        <translation type="unfinished"></translation>
+        <translation>Merhaba!</translation>
     </message>
     <message>
         <location filename="../qml/modules/Opal/SupportMe/SupportDialog.qml" line="10"/>
         <source>Thank you for using my little app! Maybe you can contribute back?</source>
-        <translation type="unfinished"></translation>
+        <translation>Küçük uygulamamı kullandığınız için teşekkür ederim! Belki siz de katkıda bulunabilirsiniz?</translation>
     </message>
     <message>
         <location filename="../qml/modules/Opal/SupportMe/SupportDialog.qml" line="11"/>
         <source>Thank you for your support!</source>
-        <translation type="unfinished"></translation>
+        <translation>Desteğiniz için teşekkür ederiz!</translation>
     </message>
     <message>
         <location filename="../qml/modules/Opal/SupportMe/SupportDialog.qml" line="55"/>
         <source>Remind me later</source>
-        <translation type="unfinished"></translation>
+        <translation>Daha sonra hatırlat</translation>
     </message>
     <message>
         <location filename="../qml/modules/Opal/SupportMe/SupportDialog.qml" line="57"/>
         <source>Don&apos;t ask me again</source>
-        <translation type="unfinished"></translation>
+        <translation>Bir daha sorma</translation>
     </message>
 </context>
 <context>
@@ -466,23 +466,23 @@
     <message>
         <location filename="../qml/harbour-parkingchaos.qml" line="80"/>
         <source>Won!</source>
-        <translation type="unfinished"></translation>
+        <translation>Kazandın!</translation>
     </message>
     <message>
         <location filename="../qml/harbour-parkingchaos.qml" line="101"/>
         <source>Restart</source>
-        <translation type="unfinished"></translation>
+        <translation>Baştan başlat</translation>
     </message>
     <message>
         <location filename="../qml/harbour-parkingchaos.qml" line="105"/>
         <source>Next level</source>
-        <translation type="unfinished"></translation>
+        <translation>Gelecek seviye</translation>
     </message>
     <message numerus="yes">
         <location filename="../qml/harbour-parkingchaos.qml" line="89"/>
         <source>You completed this level in %L1 move(s)!</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Bu seviyeyi %L1 kadar hareketle tamamladın!</numerusform>
         </translation>
     </message>
 </context>
